f981b2dc9f9ccbf66f6f7bb93e41d72fcf65d8d9
aliyu bin ahmad <aliyubinahmad2022@gmail.com>
2025-10-14 12:39:43 +0100

    Fixed database configuration

--- tailoring_website/settings.py
+++ tailoring_website/settings.py
@@ -41,6 +41,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+
 ROOT_URLCONF = 'tailoring_website.urls'
 
 TEMPLATES = [
@@ -63,13 +64,21 @@ WSGI_APPLICATION = 'tailoring_website.wsgi.application'
 # --------------------------------------------------
 # Database (Render PostgreSQL or local SQLite)
 # --------------------------------------------------
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ['DATABASE_URL'],
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # --------------------------------------------------
 # Password validation
