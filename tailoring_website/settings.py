919dd5e2c7c03478ea80e5ae8e97f9b50b18a5eb
aliyu bin ahmad <aliyubinahmad2022@gmail.com>
2025-10-14 15:22:20 +0100

    Allowing-host for  deployment

--- tailoring_website/settings.py
+++ tailoring_website/settings.py
@@ -15,7 +15,7 @@ DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.environ.get(
     'ALLOWED_HOSTS',
-    '127.0.0.1,localhost'
+    '127.0.0.1,localhost,hidar-tailoring-shop.onrender.com'
 ).split(',')
 
 # --------------------------------------------------
