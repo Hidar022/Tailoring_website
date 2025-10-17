f3b9df4ece0aca1abb408dcb4c209cdc784f1bfe
aliyu bin ahmad <aliyubinahmad2022@gmail.com>
2025-10-17 17:52:36 +0100

    Fixed missing admin app in INSTALLED_APPS

--- tailoring_website/settings.py
+++ tailoring_website/settings.py
@@ -32,6 +32,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main_app',  # ✅ main app
+    'cloudinary_storage',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
